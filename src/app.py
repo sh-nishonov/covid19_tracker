35f745d513f8acf81573ba2ecfa96cde6170cfe8
sh-nishonov <nishonovshohjahon1@gmail.com>
2022-02-01 22:24:15 +0500

    Use pathlib

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ from APICall import get_data
 from CRUD import update_realtime
 from data_manipulation import manipulate_realtime_info
 import streamlit as st
+from pathlib import Path
 
 
 # @st.cache(hash_funcs={"pymongo.mongo_client.MongoClient": id})
@@ -21,8 +22,10 @@ def main():
     # df = from_collection_to_df(collection_realtime_info)
     # df.to_csv("../data/realtime_info.csv")
     # respond = create(collection_realtime_info, data)
+    geojson_path = Path(__file__).parent[1]
+    st.write(geojson_path)
     fig = manipulate_realtime_info(
-        "../data/countries.geojson", db_name="covid19", collection="realtime_info"
+        path_geojson="../data/countries.geojson", db_name="covid19", collection="realtime_info"
     )
     st.plotly_chart(fig)
 
